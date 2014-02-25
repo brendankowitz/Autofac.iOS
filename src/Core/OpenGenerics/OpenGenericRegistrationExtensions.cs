1224e718c982e3123da01096a1cb535490c4bfd9
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-02-25 16:43:09 +1000

    Mods to get autofac compiling for Xamarin/iOS

---
+++ src/Core/OpenGenerics/OpenGenericRegistrationExtensions.cs
@@ -0,0 +1,55 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2011 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Globalization;
+using Autofac.Builder;
+using Autofac.Core;
+
+namespace Autofac.Features.OpenGenerics
+{
+    static class OpenGenericRegistrationExtensions
+    {
+        public static IRegistrationBuilder<object, ReflectionActivatorData, DynamicRegistrationStyle>
+            RegisterGeneric(ContainerBuilder builder, Type implementor)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+            if (implementor == null) throw new ArgumentNullException("implementor");
+
+			throw new NotSupportedException ("Not supported in iOS build.");
+        }
+
+        public static IRegistrationBuilder<object, OpenGenericDecoratorActivatorData, DynamicRegistrationStyle>
+            RegisterGenericDecorator(ContainerBuilder builder, Type decoratorType, Type decoratedServiceType, object fromKey, object toKey)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+            if (decoratorType == null) throw new ArgumentNullException("decoratorType");
+            if (decoratedServiceType == null) throw new ArgumentNullException("decoratedServiceType");
+
+			throw new NotSupportedException ("Not supported in iOS build.");
+        }
+			
+    }
+}
